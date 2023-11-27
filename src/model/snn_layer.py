5c95962e49d3bab663fb397fb8a47b99e4a23e28
dipezed <dcalmand73@gmail.com>
2023-11-27 10:27:18 +0100

    snn_layer over

--- src/model/snn_layer.py
+++ src/model/snn_layer.py
@@ -22,8 +22,17 @@ class snn_layer(tf.keras.layers.Layer):
             )
     def call(inputs):
         # inputs shape (batch, pixel, nb_bit)
+        batch = tf.shape(inputs)[0]
+        time_step = tf.shape(inputs)[2]
         h = tf.einsum("abc,bd->acd", inputs, self.weights)
-        for t in range(inputs.shape[-1]):
-            spiking_function(h[t])
-
-    
+        membrane_potential = tf.zeros((batch, time_step))
+        synapse = tf.zeros((batch, time_step))
+        outputs = [] 
+        for t in range(time_step):
+            s_t = spiking_function(membrane_potential - self.threshold)
+            i_t_1 = self.alpha * synapse + h[:, t]
+            v_t_1 = (self.beta * membrane_potential + synapse) * (self.threshold - s_t)
+            outputs.append(s_t)
+            membrane_potential = v_t_1
+            synapse = s_t
+        return outputs
