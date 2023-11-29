78cfe986d7bb861c7c06b6cfc680c5a8f0494d31
dipezed <dcalmand73@gmail.com>
2023-11-29 22:22:54 +0100

    testing the new branch

--- src/pretreatment/encode.py
+++ src/pretreatment/encode.py
@@ -5,6 +5,7 @@ def encode(data):
     data = data.numpy()
     res = np.unpackbits(data, axis=1)
     res = res.reshape((data.shape[0], data.shape[1], 8))
+
     return res.astype(np.float32)
 def encode_y(y):
     y = y.numpy()
